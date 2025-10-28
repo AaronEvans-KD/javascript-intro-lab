6079595856bc5f3d8d8727d7fa94a56b07114a10
Aaron Evans <sydlxia@gmail.com>
2025-10-28 10:44:48 +0000

    commit 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.dir(document);
+const myHeading = document.getElementById('myHeading');
+myHeading.innerText = "Aaron"
+const firstLink= document.querySelector('nav ul li a');
+firstLink.setAttribute('href', 'https://www.google.com');
+const colourPickers = document.querySelectorAll(".colpicker");
+console.dir(colourPickers);
+for (let i = 0; i < colourPickers.length; i++) {
+  colourPickers[i].style.backgroundColor = "red";
+}   
+
+document.getElementById("myTestBtn").addEventListener("click", (ev)=>{
+  console.info("Button clicked");
+});
+    console.info(ev.target);
+
+    const redBtn = document.querySelector(".red");
+    redBtn.addEventListener("click", function() {
+      document.body.style.backgroundColor = "redback";
+    });
+
+    const greenBtn = document.querySelector(".green");
+    greenBtn.addEventListener("click", function() {
+      document.body.style.backgroundColor = "greenback";
+    });
+
+    const blueBtn = document.querySelector(".blue");
+    blueBtn.addEventListener("click", function() {
+      document.body.style.backgroundColor = "blueback";
+    });
